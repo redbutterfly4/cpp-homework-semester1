fb6b2a37ae05e863dbb5b7a7f54410dcb17aff7e
Nikita Lukyanovich <papilionoidea4@gmail.com>
2024-09-28 19:07:38 +0300

    Add a check that the denominator is not equal to 0 in seminar01/task3

--- seminar01/task3.cpp
+++ seminar01/task3.cpp
@@ -14,6 +14,10 @@ int main()
     // Calculate z using a given formula
     double zNumerator = x + ((2.0 + y) / (x * x));
     double zDenominator = y + 1.0 / std::sqrt(x * x + 10);
+    if (zDenominator == 0.0) {
+        std::cout << "Denominator of z cannot be equal to 0." << std::endl;
+        return 0;
+    }
     double z = zNumerator / zDenominator;
 
     // Calculate q using a given formula
